c008c7ecff29b65f73e6f785b0bdf4dd83d45b97
Maninderpalsingh9501 <67618413+Maninderpalsingh9501@users.noreply.github.com>
2025-11-03 12:58:36 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput
+#!/usr/bin/env bash
+set -e
+
+# install dependencies
+pip install -r requirements.txt
+
+# collect static into STATIC_ROOT
+python manage.py collectstatic --noinput
+
+# (optional) run migrations if you use an external DB
+# python manage.py migrate --noinput
